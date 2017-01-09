456c2c7e6231f8d76eb8d185e7e749c90a9bfb5c
korisnik <malik.bisic@gmail.com>
2017-01-09 18:24:44 +0100

    Parse commit 2

---
+++ app/src/main/java/StarterApplication.java
@@ -0,0 +1,25 @@
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by korisnik on 09/01/2017.
+ */
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("8Tcjpkpz8DG6jVyG8nlI0veWySCeTkJamXha97U1")
+                .clientKey("JfAt9jhLWz3cU1sXNgTjSoSGaEIC8neD33DXpdM1")
+                .server("https://parseapi.back4app.com")
+
+
+
+        .build()
+        );
+    }
+}
