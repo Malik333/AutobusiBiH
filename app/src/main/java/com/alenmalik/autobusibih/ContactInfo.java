44ecf1da38c5e8113aa65ed3016549d11b4e46c2
korisnik <malik.bisic@gmail.com>
2017-02-19 17:34:33 +0100

    slike, sve vračeno

--- app/src/main/java/com/alenmalik/autobusibih/ContactInfo.java
+++ app/src/main/java/com/alenmalik/autobusibih/ContactInfo.java
@@ -1,17 +1,39 @@
 package com.alenmalik.autobusibih;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ContactInfo extends AppCompatActivity {
+public class ContactInfo extends AppCompatActivity implements View.OnClickListener {
+
+    EditText imeIPrezime;
+    EditText redoslijedVoznje;
+    Button posalji;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_info);
+
+        imeIPrezime = (EditText) findViewById(R.id.imeIPrezimeEdit);
+        redoslijedVoznje = (EditText) findViewById(R.id.redoslijedVoznjeEdit);
+        posalji = (Button) findViewById(R.id.posaljiBtn);
+        posalji.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","alenmalik43@gmail.com",null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT,"Hello");
+        emailIntent.putExtra(Intent.EXTRA_TEXT,  imeIPrezime.getText().toString() +"\n\n" + redoslijedVoznje.getText().toString());
+        startActivity(Intent.createChooser(emailIntent,"Send email..."));
+    }
     @Override
     public void onBackPressed() {
         super.onBackPressed();
