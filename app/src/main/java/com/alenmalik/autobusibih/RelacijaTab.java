2c5794530159dde59d2a6dbf986eb32cfc72e2cc
alendev <tutarisalen@gmail.com>
2017-02-19 20:56:40 +0100

    neki dizajn dosadni

--- app/src/main/java/com/alenmalik/autobusibih/RelacijaTab.java
+++ app/src/main/java/com/alenmalik/autobusibih/RelacijaTab.java
@@ -1,21 +1,32 @@
 package com.alenmalik.autobusibih;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 /**
  * Created by korisnik on 18/02/2017.
  */
 
 public class RelacijaTab extends Fragment {
+    Spinner fromCitySpinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.relacija_city_traffic, container, false);
+
+
+
         return rootView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
