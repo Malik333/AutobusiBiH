f8410afd68e60f2eb36866b00b0e41cbc5062d74
korisnik <malik.bisic@gmail.com>
2017-02-22 18:53:46 +0100

    cityTraffic dizajn

---
+++ app/src/main/java/com/alenmalik/autobusibih/PopUpActivitiy.java
@@ -0,0 +1,13 @@
+package com.alenmalik.autobusibih;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopUpActivitiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_activitiy);
+    }
+}
